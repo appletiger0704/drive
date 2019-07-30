db78c88d189164840a5d0b2f3f92a7f3ddeb263d
jason hsu <appletiger0704@gmail.com>
2019-07-30 10:49:32 +0800

    second commit

--- drive.py
+++ drive.py
@@ -1,9 +1,14 @@
 C = input('來自哪個國家:')
 Y = input('請書輸入年齡:')
 Y = int(Y)
-if C == '台灣' :
-	if Y >= 18 :
+if C == '台灣':
+	if Y >= 18:
 		print('可以考駕照')
+	else:
+	    print('你還不能考駕')
+elif C == '美國':
+	if Y >= 16 :
+		print('可以開車')
 	else :
-	    print('你還不能考駕照')
-
+		print('不能開車')
+	
